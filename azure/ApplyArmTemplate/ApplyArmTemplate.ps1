8b01cd2480e144b909bf40a3276bee19c807b68a
Derek-Ashmore <dashmore@force66.com>
2020-02-05 11:28:55 -0500

    Add apply template

---
+++ azure/ApplyArmTemplate/ApplyArmTemplate.ps1
@@ -0,0 +1,17 @@
+#$subscription = "d6ec19c6-066b-4041-ac2a-99a305810922"
+#Login-AzAccount
+#Set-AzContext -SubscriptionId $subscription
+
+# ./ApplyArmTemplate VF-AZDEV-VC05-MGMT-NET-RG Dev-Mgmt/template.json
+# ./ApplyArmTemplate VF-AZDEV-VC05-APPS-NET-RG Dev-Apps/template.json
+
+$ResourceGroupName = $Args[0]
+$templateFilePath = $Args[1]
+
+#     -TemplateParameterFile  $templateParameterFile `
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $ResourceGroupName `
+    -TemplateFile $templateFilePath `
+    -Mode "Incremental" 
+
+#Write-Host $ErrorMessages
