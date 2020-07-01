9e007efe912f27f066eb79f470411b87dbfe3935
Derek-Ashmore <dashmore@force66.com>
2020-07-01 06:18:53 -0400

    VM Report initial

---
+++ azure/Reporting/Virtual-Machines/VMReport.ps1
@@ -0,0 +1,34 @@
+#Connect to an Azure Account
+#   Connect-AzAccount
+
+#Provide the subscription Id where the VMs reside
+$subscriptionId = "079fed67-2834-4fc9-bc04-3bdd5fd11228"
+
+#Provide the name of the csv file to be exported
+$reportName = "virtualMachines.csv"
+
+Select-AzSubscription $subscriptionId
+$report = @()
+$vms = Get-AzVM -Status
+$publicIps = Get-AzPublicIpAddress 
+$nics = Get-AzNetworkInterface | ?{ $_.VirtualMachine -NE $null} 
+foreach ($nic in $nics) { 
+    $info = "" | Select VmName, ResourceGroupName, Region, VmSize, VirturalNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress 
+    $vm = $vms | ? -Property Id -eq $nic.VirtualMachine.id 
+    foreach($publicIp in $publicIps) { 
+        if($nic.IpConfigurations.id -eq $publicIp.ipconfiguration.Id) {
+            $info.PublicIPAddress = $publicIp.ipaddress
+            } 
+        } 
+        $info.OsType = $vm.StorageProfile.OsDisk.OsType 
+        $info.VMName = $vm.Name 
+        $info.ResourceGroupName = $vm.ResourceGroupName 
+        $info.Region = $vm.Location 
+        $info.VmSize = $vm.HardwareProfile.VmSize
+        $info.VirturalNetwork = $nic.IpConfigurations.subnet.Id.Split("/")[-3] 
+        $info.Subnet = $nic.IpConfigurations.subnet.Id.Split("/")[-1] 
+        $info.PrivateIpAddress = $nic.IpConfigurations.PrivateIpAddress 
+        $report+=$info 
+    } 
+$report | ft VmName, ResourceGroupName, Region, VmSize, VirturalNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress 
+$report | Export-CSV "$home/$reportName"
