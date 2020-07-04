e7dad5a29a6a312dbaa3a1658b15bb5f18c024b3
Derek-Ashmore <dashmore@force66.com>
2020-07-03 22:16:51 -0400

    Added performance reporting

---
+++ azure/Reporting/Virtual-Machines/VMPerformance.ps1
@@ -0,0 +1,115 @@
+#Connect to an Azure Account
+#   Connect-AzAccount
+
+#Provide the subscription Id where the VMs reside
+$subscriptionId = "079fed67-2834-4fc9-bc04-3bdd5fd11228"
+$metricStartDate = "2020-06-01"
+$metricEndDate = "2020-06-30"
+$metricTimeGranularity = "00:05:00"
+$metricNames = "Percentage CPU,OS Disk Read Bytes/sec,OS Disk Write Bytes/sec,Data Disk Read Bytes/sec,Data Disk Write Bytes/sec"
+$cpuOffset = 0
+$osDiskReadOffset = 1
+$osDiskWriteOffset = 2
+$dataDiskReadOffset = 3
+$dataDiskWriteOffset = 4
+
+#Provide the name of the csv file to be exported
+$reportName = "virtualMachinePerformance.csv"
+
+function Get-Metric-Average($dataArray) {
+    $Total = 0
+    $Count = 0
+    if ($dataArray.Length -gt 0) {
+        foreach ($data in $dataArray) {
+            if ($data.Average -gt 0) {
+                $Total += $data.Average
+                $Count = $Count + 1
+            }    
+        }    
+        if ($Count -gt 0) {
+            return $Total / $Count
+        } else {
+            return "na"
+        }
+        
+    }
+    else {
+        return "na"
+    }
+     
+}
+
+function Get-Metric-Minimum($dataArray) {
+    $Minimum = 9999999
+    if ( $dataArray.Length -gt 0 ) {
+        foreach ($data in $dataArray) {
+            if ( ($data.Average -gt 0) -and ($data.Average -lt $Minimum) ) {
+                $Minimum = $data.Average
+            }
+        }    
+        return $Minimum
+    }
+    else {
+        return "na"
+    }
+     
+}
+
+function Get-Metric-Maximum($dataArray) {
+    $Maximum = 0
+    if ($dataArray.Length -gt 0) {
+        foreach ($data in $dataArray) {
+            if (($data.Average -gt 0) -and ($data.Average -gt $Maximum) ) {
+                $Maximum = $data.Average
+            }
+        }    
+        return $Maximum
+    }
+    else {
+        return "na"
+    }
+     
+}
+
+Select-AzSubscription $subscriptionId
+$report = @()
+$vms = Get-AzVM -Status
+foreach ($vm in $vms) { 
+    $info = "" | Select VmName, ResourceGroupName, Region, VmSize, Status, AverageCpuPct, MinCpuPct, MaxCpuPct, AverageOsIIopsRead, MinOsIIopsRead, MaxOsIIopsRead, AverageOsIIopsWrite, MinOsIIopsWrite, MaxOsIIopsWrite, AverageDataIIopsRead, MinDataIIopsRead, MaxDataIIopsRead, AverageDataIIopsWrite, MinDataIIopsWrite, MaxDataIIopsWrite
+
+        $info.VMName = $vm.Name 
+        $info.ResourceGroupName = $vm.ResourceGroupName 
+        $info.Region = $vm.Location 
+        $info.VmSize = $vm.HardwareProfile.VmSize
+        $info.Status = $vm.PowerState
+
+        $metrics = Get-AzMetric -ResourceId $vm.Id -StartTime $metricStartDate -EndTime $metricEndDate -DetailedOutput -TimeGrain $metricTimeGranularity -MetricName $metricNames
+
+        $info.MinCpuPct = Get-Metric-Minimum($metrics[$cpuOffset].Data)
+        $info.MaxCpuPct = Get-Metric-Maximum($metrics[$cpuOffset].Data)
+        $info.AverageCpuPct = Get-Metric-Average($metrics[$cpuOffset].Data)
+
+        $info.MinCpuPct = Get-Metric-Minimum($metrics[$cpuOffset].Data)
+        $info.MaxCpuPct = Get-Metric-Maximum($metrics[$cpuOffset].Data)
+        $info.AverageCpuPct = Get-Metric-Average($metrics[$cpuOffset].Data)
+
+        $info.AverageOsIIopsRead = Get-Metric-Average($metrics[$osDiskReadOffset].Data)
+        $info.MinOsIIopsRead = Get-Metric-Minimum($metrics[$osDiskReadOffset].Data)
+        $info.MaxOsIIopsRead = Get-Metric-Maximum($metrics[$osDiskReadOffset].Data)
+        
+        $info.AverageOsIIopsWrite = Get-Metric-Average($metrics[$osDiskWriteOffset].Data)
+        $info.MinOsIIopsWrite = Get-Metric-Minimum($metrics[$osDiskWriteOffset].Data)
+        $info.MaxOsIIopsWrite = Get-Metric-Maximum($metrics[$osDiskWriteOffset].Data)
+        
+        $info.AverageDataIIopsRead = Get-Metric-Average($metrics[$dataDiskReadOffset].Data)
+        $info.MinDataIIopsRead = Get-Metric-Minimum($metrics[$dataDiskReadOffset].Data)
+        $info.MaxDataIIopsRead = Get-Metric-Maximum($metrics[$dataDiskReadOffset].Data)
+        
+        $info.AverageDataIIopsWrite = Get-Metric-Average($metrics[$dataDiskWriteOffset].Data)
+        $info.MinDataIIopsWrite = Get-Metric-Minimum($metrics[$dataDiskWriteOffset].Data)
+        $info.MaxDataIIopsWrite = Get-Metric-Maximum($metrics[$dataDiskWriteOffset].Data)
+        
+        $report+=$info 
+    } 
+$report | ft VmName, ResourceGroupName, Region, VmSize, Status, AverageCpuPct, MinCpuPct, MaxCpuPct, AverageOsIIopsRead, MinOsIIopsRead, MaxOsIIopsRead, AverageOsIIopsWrite, MinOsIIopsWrite, MaxOsIIopsWrite, AverageDataIIopsRead, MinDataIIopsRead, MaxDataIIopsRead, AverageDataIIopsWrite, MinDataIIopsWrite, MaxDataIIopsWrite 
+$report | Export-CSV "$home/$reportName"
--- azure/Reporting/Virtual-Machines/VMReport.ps1
+++ azure/Reporting/Virtual-Machines/VMReport.ps1
@@ -3,6 +3,8 @@
 
 #Provide the subscription Id where the VMs reside
 $subscriptionId = "079fed67-2834-4fc9-bc04-3bdd5fd11228"
+$costStartDate = "2020-06-01"
+$costEndDate = "2020-06-30"
 
 #Provide the name of the csv file to be exported
 $reportName = "virtualMachines.csv"
@@ -11,10 +13,12 @@ Select-AzSubscription $subscriptionId
 $report = @()
 $vms = Get-AzVM -Status
 $publicIps = Get-AzPublicIpAddress 
+$costs = Get-AzConsumptionUsageDetail -StartDate $costStartDate -EndDate $costEndDate
 $nics = Get-AzNetworkInterface | ?{ $_.VirtualMachine -NE $null} 
 foreach ($nic in $nics) { 
-    $info = "" | Select VmName, ResourceGroupName, Region, VmSize, Status, VirturalNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress 
+    $info = "" | Select VmName, ResourceGroupName, Region, VmSize, Status, ComputeCost, VirturalNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress 
     $vm = $vms | ? -Property Id -eq $nic.VirtualMachine.id 
+    $vmCost = $costs | ? -Property InstanceId -eq $nic.VirtualMachine.id
     foreach($publicIp in $publicIps) { 
         if($nic.IpConfigurations.id -eq $publicIp.ipconfiguration.Id) {
             $info.PublicIPAddress = $publicIp.ipaddress
@@ -26,10 +30,18 @@ foreach ($nic in $nics) {
         $info.Region = $vm.Location 
         $info.VmSize = $vm.HardwareProfile.VmSize
         $info.Status = $vm.PowerState
+
+        $info.ComputeCost = 0.0
+        if ($vmCost.length -gt 0) {
+            foreach ($k in $vmCost) {
+                $info.ComputeCost += $k.PretaxCost
+            }
+        } 
+        #$info.ComputeCost = $vmCost[0].PretaxCost
         $info.VirturalNetwork = $nic.IpConfigurations.subnet.Id.Split("/")[-3] 
         $info.Subnet = $nic.IpConfigurations.subnet.Id.Split("/")[-1] 
         $info.PrivateIpAddress = $nic.IpConfigurations.PrivateIpAddress 
         $report+=$info 
     } 
-$report | ft VmName, ResourceGroupName, Region, VmSize, Status, VirturalNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress 
+$report | ft VmName, ResourceGroupName, Region, VmSize, Status, ComputeCost, VirturalNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress 
 $report | Export-CSV "$home/$reportName"
